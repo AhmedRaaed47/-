2ff4dd8d26bd60de2795918fc2fe62b109ea4080
AhmedRaaed47 <60524241+AhmedRaaed47@users.noreply.github.com>
2020-02-09 16:09:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("676053565969530883")
 setInterval(function() {
-channel.send(`#credit`);
+channel.send(`credit`);
 }, 30)
 })
 
