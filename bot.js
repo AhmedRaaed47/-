b725f59f1ffd67e656c890a1bfd08a5cf8ac0cec
AhmedRaaed47 <60524241+AhmedRaaed47@users.noreply.github.com>
2020-02-09 15:32:27 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("676053565969530883")
+setInterval(function() {
+channel.send(`#credit`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
